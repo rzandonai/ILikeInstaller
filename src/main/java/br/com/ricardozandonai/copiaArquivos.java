8369e611c0695f7b23e3e10d349290fe81449a97
tripeh <squall@outlook.com>
2015-11-04 08:47:46 -0200

    adicionando o metodo para descompactar zip

--- src/main/java/br/com/ricardozandonai/copiaArquivos.java
+++ src/main/java/br/com/ricardozandonai/copiaArquivos.java
@@ -1,4 +1,23 @@
 package br.com.ricardozandonai;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
+
+import br.com.ricardozandonai.utils.FileUtils;
+
 /**
  * Utilities to get information about directorys of instalation.
  * 
@@ -8,4 +27,41 @@ package br.com.ricardozandonai;
  */
 public class copiaArquivos {
 
+	public static void main(String[] args) throws URISyntaxException {
+		try {
+			wildflydemo("c:\\wilddemo\\", true, false);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static boolean wildflydemo(String dirInstall, boolean installWF, boolean isUpdate)
+			throws IOException, URISyntaxException {
+		boolean itsOK = false;
+		if (installWF) {
+			File dirInst = new File(dirInstall);
+			if (!dirInst.exists()) {
+				dirInst.mkdirs();
+			}
+			URL url = copiaArquivos.class.getResource("/arquivos");
+			if (url == null) {
+				// error - missing folder
+			} else {
+				File dir = null;
+				dir = new File(url.toURI());
+
+				for (File nextFile : dir.listFiles()) {
+					if (nextFile.getName().startsWith("wildfly-10.0.0.CR4.zip")) {
+						Files.copy(Paths.get(nextFile.getPath()), Paths.get(dirInstall + "wildfly-10.0.0.CR4.zip"),
+								StandardCopyOption.REPLACE_EXISTING);	
+						FileUtils.unZipIt(dirInstall + "wildfly-10.0.0.CR4.zip", dirInstall);
+					}
+				}
+			}
+		}
+		return itsOK;
+
+	}
 }
---
+++ src/main/java/br/com/ricardozandonai/utils/FileUtils.java
@@ -0,0 +1,62 @@
+package br.com.ricardozandonai.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.IOUtils;
+
+public class FileUtils {
+
+	List<String> fileList;
+
+	/**
+	 * Unzip it
+	 * 
+	 * @param zipFile
+	 *            input zip file
+	 * @param output
+	 *            zip file output folder
+	 */
+	public static void unZipIt(String zip, String outputFolder) {
+
+		ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile(zip);
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = entries.nextElement();
+				File entryDestination = new File(outputFolder, entry.getName());
+				if (entry.getName().startsWith("."));
+				if (entry.isDirectory())
+					entryDestination.mkdirs();
+				else {
+					entryDestination.getParentFile().mkdirs();
+					InputStream in = zipFile.getInputStream(entry);
+					OutputStream out = new FileOutputStream(entryDestination);
+					IOUtils.copy(in, out);
+					IOUtils.closeQuietly(in);
+					out.close();
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				zipFile.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
